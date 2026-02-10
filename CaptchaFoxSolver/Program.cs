97fc69cfdced52d807fc587cbf344dcd3de94489
toxicboosts1804 <toxicboosts1804@gmail.com>
2026-02-11 12:12:16 +1300

    Update Program.cs

--- CaptchaFoxSolver/Program.cs
+++ CaptchaFoxSolver/Program.cs
@@ -33,8 +33,8 @@ public class Program
             };
             File.WriteAllText("Config.json", JsonSerializer.Serialize(Config, new JsonSerializerOptions { WriteIndented = true}));
 
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
+            // Console.WriteLine("Press any key to continue...");
+            // Console.ReadKey();
 
             Environment.Exit(0);
         }
